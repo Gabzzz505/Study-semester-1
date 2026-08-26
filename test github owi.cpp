084de432c85c6075ff9de2782240ad8610859769
Gabzzz505 <gabrielantonipw@gmail.com>
2026-08-26 14:37:58 +0700

    Update and rename test github.cpp to test github owi.cpp

---
+++ test github owi.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int owi;
+	scanf("%d", &owi);
+	printf("%d\n", owi);
+	prntf("dihina hina saya diam\n");
+	return 0;
+}
--- test github.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	char [101]=x;
-	scanf("%lf", &x);
-	printf("%lf\n", x);
-	return 0;
-}
